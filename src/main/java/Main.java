cbc20e301b93915851849fd3bf1b895199a39f80
zimkamartin <martin.zimka@gmail.com>
2025-02-04 15:57:02 +0100

    ADD: ESTABLISHING THE VALIDATOR. Computing a = SHAKE-128(seed).

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
+import com.github.aelstad.keccakj.core.KeccakSponge;
+import com.github.aelstad.keccakj.fips202.Shake128;
 import com.swiftcryptollc.crypto.provider.KyberJCE;
 import com.swiftcryptollc.crypto.provider.KyberUniformRandom;
 import com.swiftcryptollc.crypto.provider.kyber.KyberParams;
 
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.util.Arrays;
@@ -11,17 +11,13 @@ import java.util.Arrays;
 import static com.swiftcryptollc.crypto.provider.kyber.Indcpa.generateUniform;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        System.out.print("Just some print 01.");
-
+    public static void main(String[] args) {
         Security.setProperty("crypto.policy", "unlimited");
         Security.addProvider(new KyberJCE());
 
-        // Alice generates a KeyPair and sends her public key to Bob
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("Kyber1024");
-        System.out.print("Just some print 02.");
-
         establishValidator();
+
+        System.out.print("Everything went well...");
 
     }
 
@@ -31,14 +27,26 @@ public class Main {
             // seed <- R_q //
 
             // Create random input of bytes for generateUniform
-            byte[] seedGU = new byte[504];
+            byte[] seedGU = new byte[504];  // NO idea what should be the size
             SecureRandom sr = SecureRandom.getInstanceStrong();
             sr.nextBytes(seedGU);
 
+            // Use generateUniform = equivalent of Parse in official Kyber:
+            // "Kyber uses a deterministic approach to sample elements in Rq that are statistically close
+            // to a uniformly random distribution. For this sampling we use a function Parse"
             KyberUniformRandom uniformRandom = new KyberUniformRandom();
             generateUniform(uniformRandom, seedGU, 504, KyberParams.paramsN);
 
             short[] seed = uniformRandom.getUniformR();  // length = 384 because of KyberParams.paramsPolyBytes
+
+            // a = SHAKE-128(seed) //
+
+            byte[] buf = new byte[64];  // NO idea what should be the size
+
+            KeccakSponge xof = new Shake128();
+            xof.reset();
+            xof.getAbsorbStream().write(Utils.shortArrayToByteArray(seed));
+            xof.getSqueezeStream().read(buf);
 
         } catch (Exception ex) {
             System.out.println("generateKyberKeys Exception! [" + ex.getMessage() + "]");
