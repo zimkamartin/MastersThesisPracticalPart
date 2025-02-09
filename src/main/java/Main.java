642a6a7b2c0fa626aeb6d84fac5aae80f2071c9d
zimkamartin <martin.zimka@gmail.com>
2025-02-09 18:45:58 +0100

    ADD: COMPRESS, DECOMPRESS. Added x and x' condition check.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 
 import static com.swiftcryptollc.crypto.provider.kyber.Indcpa.generateKyberKeys;
 import static com.swiftcryptollc.crypto.provider.kyber.Poly.*;
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +18,26 @@ public class Main {
 
         System.out.print("Everything went well...");
 
+    }
+
+    // Based on the Kyber's documentation, the following must hold for functions Compress and Decompress:
+    // x' = Decompress_q(Compress_q(x, d), d)
+    // |x' - x mod^± q| ≤ round(q / w^(d+1)).
+    // Where for an even (resp. odd) positive integer \alpha, we define r' = r mod± \alpha to be the unique
+    // element r' in the range −\alpha/2 < r' ≤ \alpha/2 (resp. −(\alpha-1)/2 ≤ r' ≤ (\alpha-1)/2)
+    // such that r' = r mod \alpha.
+    private static boolean checkCompDecompResultCodition(int x, int xPrime, int d, int q) {
+        int leftSide;
+        int mod = (xPrime - x) % q;
+        if (q % 2 == 0 && mod > q/2) {  // q is even and ...
+            leftSide = abs(mod - q);
+        } else if (q % 2 == 1 && mod > (q-1)/2) {
+            leftSide = abs(mod - q);
+        } else {
+            leftSide = abs(mod);
+        }
+        int rightSide = round((float) q / (2^(d+1)));
+        return leftSide <= rightSide;
     }
 
     private static void authenticatedKeyExchange() {
