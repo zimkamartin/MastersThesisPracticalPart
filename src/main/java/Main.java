9d7b5536d5420a39c05a68fa9901e4d1bbf2c5cb
zimkamartin <martin.zimka@gmail.com>
2025-03-08 18:51:51 +0100

    ADD: ACON AREC. Copied in ACon and ARec from branch main + Added AConARec - its signature.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,8 @@ public class Main {
 
         // CompressDecompress();
 
+        AConARec();
+
         System.out.print("Everything went well...");
 
     }
@@ -84,5 +86,23 @@ public class Main {
             System.out.println("generateKyberKeys Exception! [" + ex.getMessage() + "]");
             ex.printStackTrace();
         }
+    }
+
+    public static int ACon(double k1, double sigma1, double q, double m, double g) {
+        // round(sigma_1 * q / m)
+        long roundedTerm = Math.round(sigma1 * q / m);
+        // floor(g(k_1 + roundedTerm) / q)
+        long flooredTerm = (long) Math.floor(g * (k1 + roundedTerm) / q);
+        return (int) (flooredTerm % g);
+    }
+
+    public static int ARec(double k2, double v, double q, double m, double g) {
+        // floor(m * (v / g - k_2 / q))
+        long flooredTerm = (long) Math.floor(m * (v / g - k2 / q));
+        return (int) (flooredTerm % m);
+    }
+
+    private static void AConARec() {
+
     }
 }
